4d4259f902712b98af3266f5c2b73e60b82a5684
Emiton Alves <emitonalves@gmail.com>
2023-05-21 18:00:43 -0400

    Removed boilerplate and added basic routing

---
+++ client/src/pages/CreateReport.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function CreateReport() {
+  return (
+    <div>
+      <h1>Create report</h1>
+      <Link to="/">Return to dashboard</Link>
+    </div>
+  );
+}
